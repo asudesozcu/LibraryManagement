a6d312f4690564c1f762f8655ec8bcabb5380dd6
asudesozcu <sozcuasude@gmail.com>
2025-06-06 02:57:47 +0300

    CategoryController unit test succesful

--- src/main/java/com/knf/dev/librarymanagementsystem/securityconfig/SecurityConfiguration.java
+++ src/main/java/com/knf/dev/librarymanagementsystem/securityconfig/SecurityConfiguration.java
@@ -1,24 +1,26 @@
 package com.knf.dev.librarymanagementsystem.securityconfig;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import com.knf.dev.librarymanagementsystem.service.UserService;
 
 @Configuration
-@EnableWebSecurity
-public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+@Profile("!test")
+public class SecurityConfiguration {
 
-	@Autowired
-	private UserService userService;
+	private final UserService userService;
+
+	public SecurityConfiguration(UserService userService) {
+		this.userService = userService;
+	}
 
 	@Bean
 	public BCryptPasswordEncoder passwordEncoder() {
@@ -27,23 +29,40 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
 	@Bean
 	public DaoAuthenticationProvider authenticationProvider() {
-		var auth = new DaoAuthenticationProvider();
+		DaoAuthenticationProvider auth = new DaoAuthenticationProvider();
 		auth.setUserDetailsService(userService);
 		auth.setPasswordEncoder(passwordEncoder());
 		return auth;
 	}
 
-	@Override
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.authenticationProvider(authenticationProvider());
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		http
+				.authorizeRequests()
+						.antMatchers("/h2-console/**").permitAll()
+						.antMatchers("/js/**", "/css/**", "/img/**").permitAll()
+						.antMatchers("/login").permitAll()
+						.anyRequest().authenticated()
+				.and()
+				.formLogin()
+						.loginPage("/login")
+						.permitAll()
+				.and()
+				.logout()
+						.invalidateHttpSession(true)
+						.clearAuthentication(true)
+						.logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+						.logoutSuccessUrl("/login?logout")
+						.permitAll()
+				.and()
+				.csrf()
+						.ignoringAntMatchers("/h2-console/**")
+						.disable()
+				.headers()
+						.frameOptions()
+						.sameOrigin();
+
+		return http.build();
 	}
 
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().antMatchers("/js/**", "/css/**", "/img/**").permitAll().anyRequest().authenticated()
-				.and().formLogin().loginPage("/login").permitAll().and().logout().invalidateHttpSession(true)
-				.clearAuthentication(true).logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
-				.logoutSuccessUrl("/login?logout").permitAll();
-
-	}
 }
